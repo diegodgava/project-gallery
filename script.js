af25a9411d1c9a417192d91485db1cb4c5cf654a
Diego Gava <diego.gava15@gmail.com>
2023-04-27 15:32:35 -0300

    adicionando feature

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const images = document.getElementsByClassName("image");
-
+const body = document.getElementById("background");
 let globalIndex = 0,
     last = { x: 0, y: 0 };
 
@@ -17,12 +17,12 @@ const distanceFromLast = (x, y) => {
   return Math.hypot(x - last.x, y - last.y);
 }
 
+let controle = 25
 const handleOnMove = e => {
   const slider = document.getElementById('slider');
   const sliderValue = slider.value;
 
-  // Calcular a distância mínima baseada no valor do slider
-  const distanceMin = window.innerWidth / sliderValue;
+  const distanceMin = window.innerWidth / controle;
 
   if(distanceFromLast(e.clientX, e.clientY) > distanceMin) {
     const lead = images[globalIndex % images.length],
@@ -48,7 +48,7 @@ let isDarkMode = false;
 
 darkmode.addEventListener('click', () => {
   if (isDarkMode) {
-    document.body.style.backgroundColor = 'white';
+    body.style.backgroundColor = 'white';
     document.getElementById('menu').style.backgroundColor = 'white';
     document.getElementById('menu').style.color = 'black';
     document.getElementById('sun').classList.add('animate__fadeOutUp')
@@ -61,7 +61,7 @@ darkmode.addEventListener('click', () => {
     isDarkMode = false; 
   }
   else {
-    document.body.style.backgroundColor = 'black';
+    body.style.backgroundColor = 'black';
     document.getElementById('menu').style.backgroundColor = 'black';
     document.getElementById('menu').style.color = 'white';
     document.getElementById('moon').classList.add('animate__fadeOutUp')
@@ -74,26 +74,39 @@ darkmode.addEventListener('click', () => {
     isDarkMode = true; 
   }
 });
-document.body.addEventListener('touchmove', function(event) {
+body.addEventListener('touchmove', function(event) {
   event.preventDefault();
 }, { passive: false });
 
-animate__fadeOutUp
 
-/* const plus = document.getElementById('plus')
-const modal = document.getElementById('modal')
-const modalCont = document.getElementById('modal-container')
+const background = document.getElementById("background");
 
-plus.addEventListener('click', () => {
-modal.style.display = 'flex'
-modalCont.style.display = 'flex'
 
-modalCont.addEventListener('click', () => {
-  setTimeout(function() {
-  modalCont.style.display = 'none';
-  modal.style.display = 'none';
-  }, 100);
-})
 
-})
- */
+for (let i = 0; i < images.length; i++) {
+  const image = images[i];
+  image.addEventListener("click", () => {
+    if (image.classList.contains("active")) {
+      image.classList.remove("active");
+      controle = 25
+      for (let j = 0; j < images.length; j++) {
+        const otherImage = images[j];
+        otherImage.classList.remove("opaque");
+      }
+      document.querySelector("body").classList.remove("opaque");
+    } else {
+      image.classList.add("active");
+      controle = 0
+      for (let j = 0; j < images.length; j++) {
+        const otherImage = images[j];
+        if (otherImage !== image) {
+          otherImage.classList.add("opaque");
+        }
+      }
+    }
+  });
+}
+
+
+
+
